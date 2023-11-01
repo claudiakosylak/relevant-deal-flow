3ae4c26b3b065ea074a5b110d1424c9d62836655
Your Name <claudiakosylak@gmail.com>
2023-11-01 16:42:18 +0100

    basic error validations for upload deck form

--- app/forms/upload_deck.py
+++ app/forms/upload_deck.py
@@ -1,12 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Length, Regexp
+from app.models import Startup
+import re
+
+def website_exists(form, field):
+    site = field.data
+    startup = Startup.query.filter(Startup.website == site).first()
+    if startup:
+        raise ValidationError("Website is already registered.")
 
 class UploadDeckForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
-    description = StringField('description', validators=[DataRequired()])
-    website = StringField('website', validators=[DataRequired()])
+    name = StringField('name', validators=[DataRequired(message="Please enter a name for your startup."), Length(min=3, max=40, message="Startup name must be between 3 and 40 characters long.")])
+    description = StringField('description', validators=[DataRequired(message="Please enter a description for your startup."), Length(min=30, max=500, message="Description must be between 30 and 500 characters long.")])
+    website = StringField('website', validators=[website_exists, Length(min=5, max=255, message="Please enter a website between 6 and 255 characters.")])
     deck = StringField('deck', validators=[DataRequired()])
-    founder_1 = StringField('founder_1', validators=[DataRequired()])
-    founder_2 = StringField('founder_1', validators=[DataRequired()])
-    founder_3 = StringField('founder_1', validators=[DataRequired()])
+    founder_1 = StringField('founder_1', validators=[DataRequired(message="Please enter at least one founder name."), Length(min=5, max=100, message="Founder name must be between 5 and 100 characters long.")])
+    founder_2 = StringField('founder_2')
+    founder_3 = StringField('founder_3')
--- react-app/src/components/UploadDeck/index.js
+++ react-app/src/components/UploadDeck/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./UploadDeck.module.sass";
 import { useDispatch } from "react-redux";
 import { createStartupThunk } from "../../store/startup";
@@ -14,25 +14,47 @@ const UploadDeck = () => {
     const [founder1, setFounder1] = useState("");
     const [founder2, setFounder2] = useState("");
     const [founder3, setFounder3] = useState("");
+    const [errors, setErrors] = useState({});
+    const [hasSubmitted, setHasSubmitted] = useState(false);
+    const [backendErrors, setBackendErrors] = useState({});
+
+    useEffect(() => {
+        const newErrors = {};
+        if (name === "" || name.length < 3 || name.length > 40) newErrors.name = "Startup name must be between 3 and 40 characters long.";
+        if (description === "" || description.length < 30 || description.length > 500) newErrors.description = "Please describe your startup in between 30 and 500 characters.";
+        if (website && (website.length < 5 || name.length > 255)) newErrors.website = "Website must be between 5 and 255 characters.";
+        if (deck === "") newErrors.deck = "Please upload your deck in .pdf format.";
+        if (founder1 === "" && founder2 === "" && founder3 === "") newErrors.founders = "Please enter at least one founder name.";
+        if (founder1.length < 5 || founder1.length > 100) newErrors.founder1 = "Name must be between 5 and 100 characters.";
+        if (founder2 && (founder2.length < 5 || founder2.length > 100)) newErrors.founder2 = "Name must be between 5 and 100 characters.";
+        if (founder3 && (founder3.length < 5 || founder3.length > 100)) newErrors.founder3 = "Name must be between 5 and 100 characters.";
+        setErrors(newErrors);
+    }, [name, description, website, deck, founder1, founder2, founder3])
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const formData = new FormData();
-        formData.append('name', name)
-        formData.append('description', description)
-        formData.append('website', website)
-        formData.append('deck', deck)
-        formData.append('founder_1', founder1)
-        formData.append('founder_2', founder2)
-        formData.append('founder_3', founder3)
-        dispatch(createStartupThunk(formData)).then(response => {
-            console.log("response: ", response)
-            if (response.errors) {
-                console.log(response)
-            } else {
-                history.push(`/${response.id}`)
-            }
-        })
+        if (Object.keys(errors).length === 0) {
+            console.log("ERRORS")
+            setHasSubmitted(false);
+            const formData = new FormData();
+            formData.append('name', name)
+            formData.append('description', description)
+            formData.append('website', website)
+            formData.append('deck', deck)
+            formData.append('founder_1', founder1)
+            formData.append('founder_2', founder2)
+            formData.append('founder_3', founder3)
+            dispatch(createStartupThunk(formData)).then(response => {
+                if (response.errors) {
+                    console.log(response)
+                    setBackendErrors(response.errors)
+                } else {
+                    history.push(`/${response.id}`)
+                }
+            })
+        } else {
+            setHasSubmitted(true);
+        }
     }
 
 
@@ -43,24 +65,51 @@ const UploadDeck = () => {
                 <label>
                     What is your startup name? (You can always change this later.)
                     <input value={name} type="text" onChange={(e) => setName(e.target.value)} className={styles.inputs}></input>
+                    {(hasSubmitted && errors.name) && (
+                        <p className={styles.errors}>{errors.name}</p>
+                    )}
                 </label>
                 <label>
                     Please write a short description for your startup.
                     <textarea value={description} onChange={(e) => setDescription(e.target.value)} className={styles.inputs}></textarea>
+                    {(hasSubmitted && errors.description) && (
+                        <p className={styles.errors}>{errors.description}</p>
+                    )}
                 </label>
                 <label>
                     Please enter your company's website (if you have one).
                     <input type="text" className={styles.inputs} value={website} onChange={(e) => setWebsite(e.target.value)}></input>
+                    {(hasSubmitted && errors.website) && (
+                        <p className={styles.errors}>{errors.website}</p>
+                    )}
+                    {backendErrors.website && (
+                        <p className={styles.errors}>{backendErrors.website}</p>
+                    )}
                 </label>
                 <label>
                     Upload your deck in .pdf format.
                     <input type="text" value={deck} onChange={(e) => setDeck(e.target.value)} className={styles.inputs}></input>
+                    {(hasSubmitted && errors.deck) && (
+                        <p className={styles.errors}>{errors.deck}</p>
+                    )}
                 </label>
                 <label className={styles.founders}>
                     Who are your company's founders?
+                    {(hasSubmitted && errors.founders) && (
+                        <p className={styles.errors}>{errors.founders}</p>
+                    )}
                     <input type="text" value={founder1} placeholder="Founder 1" onChange={(e) => setFounder1(e.target.value)} className={styles.inputs}></input>
+                    {(hasSubmitted && errors.founder1) && (
+                        <p className={styles.errors}>{errors.founder1}</p>
+                    )}
                     <input type="text" value={founder2} placeholder="Founder 2" onChange={(e) => setFounder2(e.target.value)} className={styles.inputs}></input>
+                    {(hasSubmitted && errors.founder2) && (
+                        <p className={styles.errors}>{errors.founder2}</p>
+                    )}
                     <input type="text" value={founder3} placeholder="Founder 3" onChange={(e) => setFounder3(e.target.value)} className={styles.inputs}></input>
+                    {(hasSubmitted && errors.founder3) && (
+                        <p className={styles.errors}>{errors.founder3}</p>
+                    )}
                 </label>
                 <button type="submit">Upload</button>
             </form>
