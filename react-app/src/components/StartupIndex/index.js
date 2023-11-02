0ed1ee3c905abf4471b23296a7cc6ba239645ac8
Your Name <claudiakosylak@gmail.com>
2023-11-02 21:39:59 +0100

    styling for startup index

--- react-app/src/components/StartupIndex/index.js
+++ react-app/src/components/StartupIndex/index.js
@@ -22,10 +22,14 @@ const StartupIndex = () => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.top}>
-                <img src={startup.picture} className={styles.image} alt={`${startup.name} image`}></img>
+                <img src={startup.picture} className={styles.image} alt={`${startup.name} image`}
+                    onError={e => { e.currentTarget.src = "https://images.unsplash.com/photo-1590098563686-06ab8778a6a7?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }
+                    }></img>
                 <div className={styles.right}>
+                    <div className={styles.right_top}>
                     <h2>{startup?.name}</h2>
                     <p className={styles.website}>{startup?.website}</p>
+                    </div>
                     <div className={styles.founders}>
                         <h3>Founders:</h3>
                         <div className={styles.founder}>{startup?.founder_1}</div>
@@ -36,8 +40,8 @@ const StartupIndex = () => {
                             <div className={styles.founder}>{startup?.founder_3}</div>
                         )}
                     </div>
-                    <a href={startup.deck} download={`${startup.name}_deck`}>Download Deck</a>
-                    <button onClick={copyToClipboard} className={styles.copy}>Copy Link</button>
+                    <a href={startup.deck} download={`${startup.name}_deck`} className={styles.download}>Download Deck</a>
+                    <button onClick={copyToClipboard} className={styles.copy}><i class="fa-solid fa-copy"></i>Copy Link</button>
                 </div>
             </div>
             <div className={styles.bottom}>
