1c942f9f5cf511db2f702bc9a3535ee8d024c7c3
Claudia Kosylak <claudiakosylak@gmail.com>
2023-12-24 13:15:15 -0800

    remove upload button for investor

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -3,54 +3,56 @@ import styles from "./Home.module.sass";
 import { useDispatch, useSelector } from "react-redux";
 import { getStartupsThunk, getUserStartupsThunk } from "../../store/startup";
 import FeedItem from "../FeedItem";
-import { Redirect, useHistory, useLocation } from "react-router-dom/cjs/react-router-dom.min";
+import {
+  Redirect,
+  useHistory,
+  useLocation,
+} from "react-router-dom/cjs/react-router-dom.min";
 
 const Home = ({ startups }) => {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    // const startups = useSelector(state => state.startup.allStartups);
-    const startupsArray = Object.values(startups);
-    const user = useSelector(state => state.session.user);
-    const location = useLocation();
+  const dispatch = useDispatch();
+  const history = useHistory();
+  // const startups = useSelector(state => state.startup.allStartups);
+  const startupsArray = Object.values(startups);
+  const user = useSelector((state) => state.session.user);
+  const location = useLocation();
 
-
-    const uploadClick = () => {
-        if (user) {
-            history.push("/upload");
-        } else {
-            history.push("/signup", { from: "upload" });
-        }
+  const uploadClick = () => {
+    if (user) {
+      history.push("/upload");
+    } else {
+      history.push("/signup", { from: "upload" });
     }
+  };
 
-    useEffect(() => {
-        dispatch(getStartupsThunk());
-        dispatch(getUserStartupsThunk());
-    }, [location.pathname])
+  useEffect(() => {
+    dispatch(getStartupsThunk());
+    dispatch(getUserStartupsThunk());
+  }, [location.pathname]);
 
-    if (!user && location.pathname === "/my-startups") return <Redirect to="/"></Redirect>
+  if ((!user || !user.default_startup) && location.pathname === "/my-startups")
+    return <Redirect to="/"></Redirect>;
 
-    return (
-        <div className={styles.wrapper}>
-            {location.pathname === "/my-startups" ? (
-                <div className={styles.intro}>
-                    My Startups
-                </div>
-            ) : (
-                <div className={styles.intro}>
-                    Startup Feed
-                </div>
-            )}
-            <div className={styles.upload} onClick={uploadClick}>
-                <i class="fa-solid fa-circle-arrow-up"></i>
-                <p>Upload Deck</p>
-            </div>
-            <div className={styles.feed}>
-                {startupsArray?.map(startup => (
-                    <FeedItem key={startup.id} startup={startup} user={user}/>
-                ))}
-            </div>
+  return (
+    <div className={styles.wrapper}>
+      {location.pathname === "/my-startups" ? (
+        <div className={styles.intro}>My Startups</div>
+      ) : (
+        <div className={styles.intro}>Startup Feed</div>
+      )}
+      {user.default_startup && (
+        <div className={styles.upload} onClick={uploadClick}>
+          <i class="fa-solid fa-circle-arrow-up"></i>
+          <p>Upload Deck</p>
         </div>
-    )
-}
+      )}
+      <div className={styles.feed}>
+        {startupsArray?.map((startup) => (
+          <FeedItem key={startup.id} startup={startup} user={user} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
